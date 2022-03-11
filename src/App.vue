96fe1c52759249d732b505f27ba5ce55139530a2
Rowan Winsemius <rowanwins@yahoo.com.au>
2022-03-11 15:33:50 +1100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img src="/banner.svg" style="width:100%">
+  <img src="/Logo.png" style="margin-bottom: 20px;">
+  <authenticator :sign-up-attributes="['email']">
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+</template>
+
+<script>
+import { Authenticator } from "@aws-amplify/ui-vue"
+
+import Amplify from 'aws-amplify'
+// import awsconfig from './aws-exports'
+
+// Amplify.configure(awsconfig)
+
+export default {
+  components: {
+    Authenticator
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@aws-amplify/ui-vue/styles.css'
+
+createApp(App).mount('#app')
