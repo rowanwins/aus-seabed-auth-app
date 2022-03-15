5e977fdca3f94527b93342d541ad54884ccfdf25
Jaleel-at-233255765155 <jaleel.mohammad@ga.gov.au>
2022-03-15 13:36:03 +0530

    added amplify directory for configurations

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ausseabedvuea585f077a585f077": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
